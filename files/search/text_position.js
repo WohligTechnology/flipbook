f80fd41b904667f941432e79e92aa60fc865486f
Rajmshah <rajwohlig@gmail.com>
2016-04-03 17:21:58 +0530

    first git

---
+++ files/search/text_position.js
@@ -0,0 +1,167 @@
+﻿
+var positionForPages = [{
+  page: 1,
+  positions: []
+}, {
+  page: 2,
+  positions: []
+}, {
+  page: 3,
+  positions: []
+}, {
+  page: 4,
+  positions: []
+}, {
+  page: 5,
+  positions: []
+}, {
+  page: 6,
+  positions: []
+}, {
+  page: 7,
+  positions: []
+}, {
+  page: 8,
+  positions: []
+}, {
+  page: 9,
+  positions: []
+}, {
+  page: 10,
+  positions: []
+}, {
+  page: 11,
+  positions: []
+}, {
+  page: 12,
+  positions: []
+}, {
+  page: 13,
+  positions: []
+}, {
+  page: 14,
+  positions: []
+}, {
+  page: 15,
+  positions: []
+}, {
+  page: 16,
+  positions: []
+}, {
+  page: 17,
+  positions: []
+}, {
+  page: 18,
+  positions: []
+}, {
+  page: 19,
+  positions: []
+}, {
+  page: 20,
+  positions: []
+},{
+  page: 21,
+  positions: []
+}, {
+  page: 22,
+  positions: []
+}, {
+  page: 23,
+  positions: []
+}, {
+  page: 24,
+  positions: []
+}, {
+  page: 25,
+  positions: []
+}, {
+  page: 26,
+  positions: []
+}, {
+  page: 27,
+  positions: []
+}, {
+  page: 28,
+  positions: []
+}, {
+  page: 29,
+  positions: []
+}, {
+  page: 30,
+  positions: []
+},{
+  page: 31,
+  positions: []
+}, {
+  page: 32,
+  positions: []
+}, {
+  page: 33,
+  positions: []
+}, {
+  page: 34,
+  positions: []
+}, {
+  page: 35,
+  positions: []
+}, {
+  page: 36,
+  positions: []
+}, {
+  page: 37,
+  positions: []
+}, {
+  page: 38,
+  positions: []
+}, {
+  page: 39,
+  positions: []
+}, {
+  page: 40,
+  positions: []
+},{
+  page: 41,
+  positions: []
+}, {
+  page: 42,
+  positions: []
+}, {
+  page: 43,
+  positions: []
+}, {
+  page: 44,
+  positions: []
+}, {
+  page: 45,
+  positions: []
+}, {
+  page: 46,
+  positions: []
+}, {
+  page: 47,
+  positions: []
+}, {
+  page: 48,
+  positions: []
+}, {
+  page: 49,
+  positions: []
+}, {
+  page: 50,
+  positions: []
+},{
+  page: 51,
+  positions: []
+}, {
+  page: 52,
+  positions: []
+}, {
+  page: 53,
+  positions: []
+}, {
+  page: 54,
+  positions: []
+}, {
+  page: 55,
+  positions: []
+}];
---
+++ javascript/config.js
@@ -0,0 +1,705 @@
+﻿
+function orgt(s) {
+  return binl2hex(core_hx(str2binl(s), s.length * chrsz));
+}
+var bookConfig = {
+  BookmarkButtonVisible: "Show",
+  searchColor: "#00ffff",
+  searchAlpha: 0.3,
+  leastSearchChar: 0,
+  loadingBackground: "#1F2232",
+  loadingCaption: "Loading",
+  loadingCaptionColor: "#DDDDDD",
+  loadingBackground: "#1F2232",
+  loadingPicture: "",
+  bgBeginColor: "#1F2232",
+  bgEndColor: "#1F2232",
+  bgMRotation: 90,
+  backGroundImgURL: "",
+  backgroundScene: "None",
+  LeftShadowWidth: 100,
+  LeftShadowAlpha: 1,
+  RightShadowWidth: 40,
+  RightShadowAlpha: 0.6,
+  pageBackgroundColor: "#FFFFFF",
+  flipshortcutbutton: "Show",
+  OriginPageIndex: 1,
+  HardPageEnable: "No",
+  BindingType: "side",
+  RightToLeft: "No",
+  flippingTime: 0.6,
+  retainBookCenter: "Yes",
+  FlipStyle: "Flip",
+  showDoublePage: "Yes",
+  totalPagesCaption: "",
+  pageNumberCaption: "",
+  topMargin: 10,
+  bottomMargin: 10,
+  leftMargin: 10,
+  rightMargin: 10,
+  appLogoIcon: "",
+  appLogoLinkURL: "",
+  HomeURL: "%first page%",
+  appLogoOpenWindow: "Blank",
+  bookTitle: "Mehta Jewellers Products Catalog",
+  bookDescription: "",
+  toolbarColor: "#444444",
+  iconColor: "#CCCCCC",
+  pageNumColor: "#333333",
+  FlipSound: "no",
+  QRCode: "Hide",
+  logoHeight: "25",
+  logoPadding: "10",
+  logoTop: "8",
+  HomeButtonVisible: "Hide",
+  HomeButtonIcon: "",
+  AnnotationButtonVisible: "Hide",
+  AnnotationButtonIcon: "",
+  ShareButtonVisible: "Show",
+  ShareButtonIcon: "",
+  ThumbnailsButtonVisible: "Show",
+  thumbnailColor: "#333333",
+  thumbnailAlpha: 70,
+  ThumbnailButtonIcon: "",
+  ZoomButtonVisible: "Show",
+  ZoomInButtonIcon: "",
+  ZoomOutButtonIcon: "",
+  FullscreenButtonVisible: "Show",
+  FullscreenButtonIcon: "",
+  ExitFullscreenButtonIcon: "",
+  BookMarkButtonVisible: "Hide",
+  TableOfContentButtonIcon: "",
+  TableOfContentButtonVisible: "Hide",
+  bookmarkBackground: "#000000",
+  bookmarkFontColor: "#cccccc",
+  BookmarkButtonIcon: "",
+  SearchButtonVisible: "Show",
+  leastSearchChar: 3,
+  searchBackground: "#383838",
+  searchFontColor: "#000000",
+  SearchButtonIcon: "",
+  PrintButtonVisible: "Show",
+  PrintButtonIcon: "",
+  printWatermarkFile: "",
+  BackgroundSoundButtonVisible: "Hide",
+  BackgroundSoundURL: "",
+  BackgroundSoundLoop: -1,
+  BackgroundSoundButtonOnIcon: "",
+  BackgroundSoundButtonOffIcon: "",
+  HelpButtonVisible: "Hide",
+  helpContentFileURL: "",
+  helpWidth: 400,
+  helpHeight: 450,
+  showHelpContentAtFirst: "No",
+  HelpButtonIcon: "",
+  aboutButtonVisible: "Hide",
+  CompanyLogoFile: "",
+  AboutButtonIcon: "",
+  AutoPlayButtonVisible: "Show",
+  autoPlayAutoStart: "No",
+  autoPlayDuration: 3,
+  autoPlayLoopCount: 1,
+  AutoPlayStartButtonIcon: "",
+  AutoPlayStopButtonIcon: "",
+  minZoomWidth: 403,
+  minZoomHeight: 518,
+  mouseWheelFlip: "yes",
+  DownloadButtonVisible: "Show",
+  DownloadButtonIcon: "",
+  DownloadURL: "",
+  VideoButtonVisible: "Hide",
+  VideoButtonIcon: "",
+  SlideshowButtonVisible: "Hide",
+  SlideshowButtonIcon: "",
+  OpenWindow: "Blank",
+  showLinkHint: "No",
+  haveAdSense: "No",
+  adSenseWidth: 200,
+  adSenseHeight: 200,
+  adSenseLeft: 50,
+  adSenseTop: 50,
+  adSenseClientId: "",
+  googleAnalyticsID: "",
+  language: "English",
+  AboutAddress: "",
+  AboutEmail: "",
+  AboutMobile: "",
+  AboutWebsite: "",
+  AboutDescription: "",
+  AboutAuthor: "Mehta Jewellers",
+  totalPageCount: 55,
+  largePageWidth: 5167,
+  largePageHeight: 7309,
+  normalPath: "files/page/",
+  largePath: "files/large/",
+  thumbPath: "files/thumb/"
+};
+var fliphtml5_pages = [{
+  "l": "files/large/1.jpg",
+  "n": "files/page/1.jpg",
+  "t": "files/thumb/1.jpg"
+}, {
+  "l": "files/large/2.jpg",
+  "n": "files/page/2.jpg",
+  "t": "files/thumb/2.jpg"
+}, {
+  "l": "files/large/3.jpg",
+  "n": "files/page/3.jpg",
+  "t": "files/thumb/3.jpg"
+}, {
+  "l": "files/large/4.jpg",
+  "n": "files/page/4.jpg",
+  "t": "files/thumb/4.jpg"
+}, {
+  "l": "files/large/5.jpg",
+  "n": "files/page/5.jpg",
+  "t": "files/thumb/5.jpg"
+}, {
+  "l": "files/large/6.jpg",
+  "n": "files/page/6.jpg",
+  "t": "files/thumb/6.jpg"
+}, {
+  "l": "files/large/7.jpg",
+  "n": "files/page/7.jpg",
+  "t": "files/thumb/7.jpg"
+}, {
+  "l": "files/large/8.jpg",
+  "n": "files/page/8.jpg",
+  "t": "files/thumb/8.jpg"
+}, {
+  "l": "files/large/9.jpg",
+  "n": "files/page/9.jpg",
+  "t": "files/thumb/9.jpg"
+}, {
+  "l": "files/large/10.jpg",
+  "n": "files/page/10.jpg",
+  "t": "files/thumb/10.jpg"
+}, {
+  "l": "files/large/11.jpg",
+  "n": "files/page/11.jpg",
+  "t": "files/thumb/11.jpg"
+}, {
+  "l": "files/large/12.jpg",
+  "n": "files/page/12.jpg",
+  "t": "files/thumb/12.jpg"
+}, {
+  "l": "files/large/13.jpg",
+  "n": "files/page/13.jpg",
+  "t": "files/thumb/13.jpg"
+}, {
+  "l": "files/large/14.jpg",
+  "n": "files/page/14.jpg",
+  "t": "files/thumb/14.jpg"
+}, {
+  "l": "files/large/15.jpg",
+  "n": "files/page/15.jpg",
+  "t": "files/thumb/15.jpg"
+}, {
+  "l": "files/large/16.jpg",
+  "n": "files/page/16.jpg",
+  "t": "files/thumb/16.jpg"
+}, {
+  "l": "files/large/17.jpg",
+  "n": "files/page/17.jpg",
+  "t": "files/thumb/17.jpg"
+}, {
+  "l": "files/large/18.jpg",
+  "n": "files/page/18.jpg",
+  "t": "files/thumb/18.jpg"
+}, {
+  "l": "files/large/19.jpg",
+  "n": "files/page/19.jpg",
+  "t": "files/thumb/19.jpg"
+}, {
+  "l": "files/large/20.jpg",
+  "n": "files/page/20.jpg",
+  "t": "files/thumb/20.jpg"
+}, {
+  "l": "files/large/21.jpg",
+  "n": "files/page/21.jpg",
+  "t": "files/thumb/21.jpg"
+}, {
+  "l": "files/large/22.jpg",
+  "n": "files/page/22.jpg",
+  "t": "files/thumb/22.jpg"
+}, {
+  "l": "files/large/23.jpg",
+  "n": "files/page/23.jpg",
+  "t": "files/thumb/23.jpg"
+}, {
+  "l": "files/large/24.jpg",
+  "n": "files/page/24.jpg",
+  "t": "files/thumb/24.jpg"
+}, {
+  "l": "files/large/25.jpg",
+  "n": "files/page/25.jpg",
+  "t": "files/thumb/25.jpg"
+}, {
+  "l": "files/large/26.jpg",
+  "n": "files/page/26.jpg",
+  "t": "files/thumb/26.jpg"
+}, {
+  "l": "files/large/27.jpg",
+  "n": "files/page/27.jpg",
+  "t": "files/thumb/27.jpg"
+}, {
+  "l": "files/large/28.jpg",
+  "n": "files/page/28.jpg",
+  "t": "files/thumb/28.jpg"
+}, {
+  "l": "files/large/29.jpg",
+  "n": "files/page/29.jpg",
+  "t": "files/thumb/29.jpg"
+}, {
+  "l": "files/large/30.jpg",
+  "n": "files/page/30.jpg",
+  "t": "files/thumb/30.jpg"
+}, {
+  "l": "files/large/31.jpg",
+  "n": "files/page/31.jpg",
+  "t": "files/thumb/31.jpg"
+}, {
+  "l": "files/large/32.jpg",
+  "n": "files/page/32.jpg",
+  "t": "files/thumb/32.jpg"
+}, {
+  "l": "files/large/33.jpg",
+  "n": "files/page/33.jpg",
+  "t": "files/thumb/33.jpg"
+}, {
+  "l": "files/large/34.jpg",
+  "n": "files/page/34.jpg",
+  "t": "files/thumb/34.jpg"
+}, {
+  "l": "files/large/35.jpg",
+  "n": "files/page/35.jpg",
+  "t": "files/thumb/35.jpg"
+}, {
+  "l": "files/large/36.jpg",
+  "n": "files/page/36.jpg",
+  "t": "files/thumb/36.jpg"
+}, {
+  "l": "files/large/37.jpg",
+  "n": "files/page/37.jpg",
+  "t": "files/thumb/37.jpg"
+}, {
+  "l": "files/large/38.jpg",
+  "n": "files/page/38.jpg",
+  "t": "files/thumb/38.jpg"
+},{
+  "l": "files/large/39.jpg",
+  "n": "files/page/39.jpg",
+  "t": "files/thumb/39.jpg"
+},{
+  "l": "files/large/40.jpg",
+  "n": "files/page/40.jpg",
+  "t": "files/thumb/40.jpg"
+},{
+  "l": "files/large/41.jpg",
+  "n": "files/page/41.jpg",
+  "t": "files/thumb/41.jpg"
+},
+{
+  "l": "files/large/42.jpg",
+  "n": "files/page/42.jpg",
+  "t": "files/thumb/42.jpg"
+},
+{
+  "l": "files/large/43.jpg",
+  "n": "files/page/43.jpg",
+  "t": "files/thumb/43.jpg"
+},
+{
+  "l": "files/large/44.jpg",
+  "n": "files/page/44.jpg",
+  "t": "files/thumb/44.jpg"
+},
+{
+  "l": "files/large/45.jpg",
+  "n": "files/page/45.jpg",
+  "t": "files/thumb/45.jpg"
+},
+{
+  "l": "files/large/46.jpg",
+  "n": "files/page/46.jpg",
+  "t": "files/thumb/46.jpg"
+},
+{
+  "l": "files/large/47.jpg",
+  "n": "files/page/47.jpg",
+  "t": "files/thumb/47.jpg"
+},
+{
+  "l": "files/large/48.jpg",
+  "n": "files/page/48.jpg",
+  "t": "files/thumb/48.jpg"
+},
+{
+  "l": "files/large/49.jpg",
+  "n": "files/page/49.jpg",
+  "t": "files/thumb/49.jpg"
+},
+{
+  "l": "files/large/50.jpg",
+  "n": "files/page/50.jpg",
+  "t": "files/thumb/50.jpg"
+},
+{
+  "l": "files/large/51.jpg",
+  "n": "files/page/51.jpg",
+  "t": "files/thumb/51.jpg"
+},
+{
+  "l": "files/large/52.jpg",
+  "n": "files/page/52.jpg",
+  "t": "files/thumb/52.jpg"
+},
+{
+  "l": "files/large/53.jpg",
+  "n": "files/page/53.jpg",
+  "t": "files/thumb/53.jpg"
+},
+{
+  "l": "files/large/54.jpg",
+  "n": "files/page/54.jpg",
+  "t": "files/thumb/54.jpg"
+},
+{
+  "l": "files/large/55.jpg",
+  "n": "files/page/55.jpg",
+  "t": "files/thumb/55.jpg"
+},
+
+];
+var language = [{
+  language: "english",
+  btnFirstPage: "First",
+  btnNextPage: "Next",
+  btnLastPage: "Last",
+  btnPrePage: "Previous",
+  btnGoToHome: "Home",
+  btnDownload: "Download",
+  btnSoundOn: "SoundOn",
+  btnSoundOff: "SoundOff",
+  btnPrint: "Print",
+  btnThumb: "Thumbnails",
+  btnBookMark: "Book Mark",
+  frmBookMark: "Book Mark",
+  btnZoomIn: "Zoom In",
+  btnZoomOut: "Zoom Out",
+  btnAutoFlip: "Auto Flip",
+  btnStopAutoFlip: "Stop Auto Flip",
+  btnSocialShare: "Share",
+  btnHelp: "Help",
+  btnAbout: "About",
+  btnSearch: "Search",
+  btnFullscreen: "Fullscreen",
+  btnExitFullscreen: "Exit Fullscreen",
+  btnMore: "More",
+  frmPrintCaption: "Print",
+  frmPrintall: "Print All Pages",
+  frmPrintcurrent: "Print Current Page",
+  frmPrintRange: "Print Range",
+  frmPrintexample: "Example: 2,3,5-10",
+  frmPrintbtn: "Print",
+  frmShareCaption: "Share",
+  frmShareLabel: "Share",
+  frmShareInfo: "You can easily share this publication to social networks.Just click the appropriate button below",
+  frminsertLabel: "Insert to Site",
+  frminsertInfo: "Use the code below to embed this publication to your website.",
+  frmaboutcaption: "Contact",
+  frmaboutcontactinformation: "Contact Information",
+  frmaboutADDRESS: "ADDRESS",
+  frmaboutEMAIL: "EMAIL",
+  frmaboutWEBSITE: "WEBSITE",
+  frmaboutMOBILE: "MOBILE",
+  frmaboutAUTHOR: "AUTHOR",
+  frmaboutDESCRIPTION: "DESCRIPTION",
+  frmSearch: "Search",
+  frmToc: "Table of Content",
+  btnTableOfContent: "Show Table of Content",
+  btnNote: "Annotation"
+}];
+var ols = [{
+  "caption": "Cover",
+  "page": 1,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "2",
+  "page": 2,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "3",
+  "page": 3,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "4",
+  "page": 4,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "5",
+  "page": 5,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "6",
+  "page": 6,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "7",
+  "page": 7,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "8",
+  "page": 8,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "9",
+  "page": 9,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "10",
+  "page": 10,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "11",
+  "page": 11,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "12",
+  "page": 12,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "13",
+  "page": 113,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "14",
+  "page": 14,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "15",
+  "page": 15,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "16",
+  "page": 16,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "17",
+  "page": 17,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "18",
+  "page": 18,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "19",
+  "page": 19,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "20",
+  "page": 20,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "21",
+  "page": 21,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "22",
+  "page": 22,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "23",
+  "page": 23,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "24",
+  "page": 24,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "25",
+  "page": 25,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "26",
+  "page": 26,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "27",
+  "page": 27,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "28",
+  "page": 28,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "29",
+  "page": 29,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "30",
+  "page": 30,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "31",
+  "page": 31,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "32",
+  "page": 32,
+  "level": 1,
+  "children": []
+},{
+  "caption": "33",
+  "page": 33,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "34",
+  "page": 34,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "35",
+  "page": 35,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "36",
+  "page": 36,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "37",
+  "page": 37,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "38",
+  "page": 38,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "39",
+  "page": 39,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "40",
+  "page": 40,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "41",
+  "page": 41,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "42",
+  "page": 42,
+  "level": 1,
+  "children": []
+},{
+  "caption": "43",
+  "page": 43,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "44",
+  "page": 44,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "45",
+  "page": 45,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "46",
+  "page": 46,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "47",
+  "page": 47,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "48",
+  "page": 48,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "49",
+  "page": 49,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "50",
+  "page": 50,
+  "level": 1,
+  "children": []
+},{
+  "caption": "51",
+  "page": 51,
+  "level": 1,
+  "children": []
+},{
+  "caption": "52",
+  "page": 52,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "53",
+  "page": 53,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "54",
+  "page": 54,
+  "level": 1,
+  "children": []
+}, {
+  "caption": "back",
+  "page": 55,
+  "level": 1,
+  "children": []
+}];
+var bmtConfig = [];
+var staticAd = {
+  haveAd: false,
+  data: []
+};
+var videoList = [];
+var slideshow = [];
